34b6e627351b3b3069be1c18d0aa4a47706dda53
Mark McCoy <mhmccoy@email.sc.edu>
2022-04-29 00:21:57 -0400

    Gil's Initial Code
    This is gil's power hour code that seems to work ok but could use some improvements including calling to game mechanics and creating a main bot function

---
+++ power_hour.py
@@ -0,0 +1,17 @@
+import os, time, discord
+
+client = discord.Client()
+my_secret = os.environ['TOKEN']
+
+@client.event
+async def on_message(message):
+    if message.content == "$hesdrinking":
+      duration = 60
+      for idx in range(0, duration):
+        time.sleep(60)
+        if idx == duration/2:
+          await message.channel.send('Half way point, don't puke now Jared')
+        await message.channel.send('DRINK!')
+
+
+client.run(my_secret)
