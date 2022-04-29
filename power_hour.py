e477da83b1fb8e4b33dd5c40113622a96d9c50fd
Mark McCoy <mhmccoy@email.sc.edu>
2022-04-29 00:29:43 -0400

    Update power_hour.py

--- power_hour.py
+++ power_hour.py
@@ -10,8 +10,6 @@ async def on_message(message):
       for idx in range(0, duration):
         time.sleep(60)
         if idx == duration/2:
-          await message.channel.send('Half way point, don't puke now Jared')
+          await message.channel.send("half way point, dont puke now Jared")
         await message.channel.send('DRINK!')
-
-
 client.run(my_secret)
