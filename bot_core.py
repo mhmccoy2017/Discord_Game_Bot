1f8fc20490dcf890106b1261f3064bd113435cda
Mark McCoy <mhmccoy@email.sc.edu>
2022-05-12 15:56:12 -0400

    Cleaning up unused code

--- bot_core.py
+++ bot_core.py
@@ -13,30 +13,3 @@ if __name__ == '__main__':
 		asyncio.run(power_hour.powerhour('powerhour'))
 	except KeyboardInterrupt:
 		sys.exit(0)
-
-'''
-class MyCli(discord.Client):
-
-	async def on_ready(self):
-		print('Logged in as')
-		print(self.user.name)
-		print(self.user.id)
-		print('------')
-
-	async def on_message(self, msg):
-		if msg.author == bot.user:
-			return
-
-		if msg.content.startswith('$drink'):
-			await msg.channel.send('Drinking with friends')
-
-
-
-if __name__ == "__main__":
-	#Getting API Token for bot and storing as local var
-	api = API_Token.bot_api().token
-	bot = MyCli()
-	#bot = commands.Bot(command_prefix='$')
-	#bot.get_cog(Power_Hour(bot))
-	bot.run(api)
-'''
