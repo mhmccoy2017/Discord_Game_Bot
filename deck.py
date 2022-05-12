9823c3ad92550d7bacc4ba9581807f4f3791868d
Mark McCoy <mhmccoy@email.sc.edu>
2022-05-12 16:53:19 -0400

    Implementing a deck of cards classes to be used for games

---
+++ deck.py
@@ -0,0 +1,72 @@
+import random
+
+class Card:
+    suits = ["Spades",
+             "Hearts",
+             "Diamonds",
+             "Clubs"]
+    
+    values = [None, None,"2", "3",
+              "4", "5", "6", "7",
+              "8", "9", "10",
+              "Jack", "Queen",
+              "King", "Ace"]
+
+    def __init__(self, v, s):
+        """suit + value are ints"""
+        self.value = v
+        self.suit = s
+
+    #Less than function	
+    def __lt__(self, c2):
+        if self.value < c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit < c2.suit:
+                return True
+            else:
+                return False
+        return False
+
+    #Greater than function    
+    def __gt__(self, c2):
+        if self.value > c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit > c2.suit:
+                return True
+            else:
+                return False
+        return False
+
+    def __repr__(self):
+        v = self.values[self.value] + " of " + self.suits[self.suit]
+        return v
+
+class Deck:
+
+	#Sets up an unshuffled deck
+    def __init__(self):
+        self.cards = []
+        for i in range(2, 15):
+            for j in range(4):
+                self.cards.append(Card(i,j))
+
+    #Will pop a card from the deck
+    def remove_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
+
+    #Shuffles the deck randomly
+    def shuffle_deck(self):
+    	random.shuffle(self.cards)
+
+   	#Will print the entire deck
+    def __str__(self):
+    	return f'{self.cards}'
+
+deck = Deck()
+print(f'Unshuffled {deck}')
+deck.shuffle_deck()
+print(f'Shuffled {deck}')
